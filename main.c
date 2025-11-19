25ee0b62bdfb1db12459e59d5355e6c505196c53
Smelly Shrimp <tymek.lamza@op.pl>
2025-11-19 19:11:32 +0100

    created ascii tree

--- main.c
+++ main.c
@@ -1,3 +1,17 @@
+#include <stdio.h>
+
+const char* TREE =
+	R"(       *       )" "\n" 
+	R"(       |       )" "\n"
+	R"(      / \      )" "\n"
+	R"(     /_ _\     )" "\n"
+	R"(     /   \     )" "\n"
+	R"(    /_   _\    )" "\n"
+	R"(    /     \    )" "\n"
+	R"(   /___ ___\   )" "\n"
+	R"(______/|\______)" "\n";
+
 int main() {
+	printf("%s\n", TREE);
 	return 0;
 }
